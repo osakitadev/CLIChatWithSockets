9aa6332d0729cb3bc4db0c7f7c568a8218654fbe
Osakita <138826243+osakitadev@users.noreply.github.com.>
2024-12-24 16:48:01 -0600

    Added server configs

---
+++ config/server_config.go
@@ -0,0 +1,8 @@
+package config
+
+const (
+	// ServerPort is the port where the server will be listening
+	ServerPort = ":8080"
+	// ServerProtocol is the protocol that the server will be using (eithet tcp or udp)
+	Protocol = "tcp"
+)
